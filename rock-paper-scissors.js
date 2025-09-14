9a2635fde202c65c453937f4c478267d28039a31
D3LTAGIRL <56973626+D3LTAGIRL@users.noreply.github.com>
2025-09-14 01:26:20 -0500

    Add logic for playing a first-to-three rock paper scissors match

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,49 +1,54 @@
 console.log("Hello Bozo")
+function playGame() {
 
-function getComputerChoice() {
-
-    // generate a random number from 0-2 (3 possibilities)
-    result = Math.floor(Math.random() * 3)
+    let humanScore, computerScore = 0;
+    while (humanScore !== 3 && computerScore !== 3) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
     
-    // choose the option that corresponds to that random number & return it
-    switch (result) {
-        case 0:
-            return "rock";
-        case 1:
-            return "paper";
-        case 2:
-            return "scissors";
+    function getComputerChoice() {
+
+        // generate a random number from 0-2 (3 possibilities)
+        result = Math.floor(Math.random() * 3)
+        
+        // choose the option that corresponds to that random number & return it
+        switch (result) {
+            case 0:
+                return "rock";
+            case 1:
+                return "paper";
+            case 2:
+                return "scissors";
+        }
+    }
+
+    function getHumanChoice() {
+        // prompt the user for a choice between rock paper or scissors
+        return prompt("rock, paper, or scissors?")
+    }
+
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+
+        if (humanChoice === computerChoice) {
+            console.log(`it's a tie! you both picked ${humanChoice}`)
+        }
+
+        if (humanChoice === "rock" && computerChoice === "scissors" ||
+            humanChoice === "paper" && computerChoice === "rock" ||
+            humanChoice === "scissors" && computerChoice === "paper"
+            ) {
+            console.log(`you won! ${humanChoice} beats ${computerChoice}`)
+            humanScore++;
+        }
+
+        if (humanChoice === "paper" && computerChoice === "scissors" ||
+            humanChoice === "scissors" && computerChoice === "rock" ||
+            humanChoice === "rock" && computerChoice === "paper"
+            ) {
+            console.log(`you lost! ${humanChoice} gets beaten by ${computerChoice}`)
+            computerScore++;
+        }
     }
 }
-
-function getHumanChoice() {
-    // prompt the user for a choice between rock paper or scissors
-    return prompt("rock, paper, or scissors?")
-}
-
-function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-
-    if (humanChoice === computerChoice) {
-        console.log(`it's a tie! you both picked ${humanChoice}`)
-    }
-
-    if (humanChoice === "rock" && computerChoice === "scissors" ||
-        humanChoice === "paper" && computerChoice === "rock" ||
-        humanChoice === "scissors" && computerChoice === "paper"
-        ) {
-        console.log(`you won! ${humanChoice} beats ${computerChoice}`)
-        humanScore++;
-    }
-
-    if (humanChoice === "paper" && computerChoice === "scissors" ||
-        humanChoice === "scissors" && computerChoice === "rock" ||
-        humanChoice === "rock" && computerChoice === "paper"
-        ) {
-        console.log(`you lost! ${humanChoice} gets beaten by ${computerChoice}`)
-        computerChoice++;
-    }
-}
-
-let humanScore, computerScore = 0;
 
