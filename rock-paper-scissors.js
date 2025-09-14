30dbe4ebc2aac7389e1334b6b8f4b6e845d8f6d5
D3LTAGIRL <56973626+D3LTAGIRL@users.noreply.github.com>
2025-09-14 01:22:50 -0500

    Add logic for playing a round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,15 +8,42 @@ function getComputerChoice() {
     // choose the option that corresponds to that random number & return it
     switch (result) {
         case 0:
-            return "r";
+            return "rock";
         case 1:
-            return "p";
+            return "paper";
         case 2:
-            return "s";
+            return "scissors";
     }
 }
 
 function getHumanChoice() {
     // prompt the user for a choice between rock paper or scissors
-    return prompt("(R)ock, (P)aper, (S)cissors!")
+    return prompt("rock, paper, or scissors?")
 }
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice === computerChoice) {
+        console.log(`it's a tie! you both picked ${humanChoice}`)
+    }
+
+    if (humanChoice === "rock" && computerChoice === "scissors" ||
+        humanChoice === "paper" && computerChoice === "rock" ||
+        humanChoice === "scissors" && computerChoice === "paper"
+        ) {
+        console.log(`you won! ${humanChoice} beats ${computerChoice}`)
+        humanScore++;
+    }
+
+    if (humanChoice === "paper" && computerChoice === "scissors" ||
+        humanChoice === "scissors" && computerChoice === "rock" ||
+        humanChoice === "rock" && computerChoice === "paper"
+        ) {
+        console.log(`you lost! ${humanChoice} gets beaten by ${computerChoice}`)
+        computerChoice++;
+    }
+}
+
+let humanScore, computerScore = 0;
+
