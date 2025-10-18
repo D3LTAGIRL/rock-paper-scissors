fe7fbaa45780815221577dca108df7f766eb4ed5
D3LTAGIRL <56973626+D3LTAGIRL@users.noreply.github.com>
2025-10-18 18:43:59 -0500

    Finish game playing logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,16 +8,24 @@ const questionImg = "img/question-icon.svg";
 const playerChoiceIcon = document.querySelector(".player-choice");
 const computerChoiceIcon = document.querySelector(".computer-choice");
 
+const playerScoreDivs = document.querySelectorAll(".player-score div");
+const computerScoreDivs = document.querySelectorAll(".computer-score div");
+
 let choiceButtons = document.querySelectorAll(".choice-buttons img");
 let confirmButton = document.querySelector("#confirm-button");
 
+let humanScore = 0;
+let computerScore = 0;
+
 choiceButtons.forEach((element) => element.addEventListener("click", updateChoice))
+
+confirmButton.addEventListener("click", () => playRound(currentChoice, getComputerChoice()));
 
 function updateChoice(event) {
     let choice = event.target.id.split("-")[0];
     console.log(currentChoice);
     if (choice !== currentChoice) {
-        choiceButtons.forEach((choiceButton) => choiceButton.classList.remove("selected"))
+        choiceButtons.forEach((choiceButton) => choiceButton.classList.remove("selected"));
         currentChoice = choice;
         event.target.classList.add("selected");
         confirmButton.removeAttribute("disabled");
@@ -31,31 +39,56 @@ function updateChoice(event) {
 
 function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
+    choiceButtons.forEach((choiceButton) => choiceButton.classList.remove("selected"));
+    confirmButton.setAttribute("disabled", "disabled");
+    currentChoice = null;
     // update choice icons
     playerChoiceIcon.src = "img/" + humanChoice + "-icon.svg";
     computerChoiceIcon.src = "img/" + computerChoice + "-icon.svg";
 
     // wait a bit
+    setTimeout(() => {
     // increase score if someone won
+        if (humanChoice === computerChoice) {
+            console.log(`it's a tie! you both picked ${humanChoice}`)
+        }
 
-    if (humanChoice === computerChoice) {
-        console.log(`it's a tie! you both picked ${humanChoice}`)
-    }
+        if (humanChoice === "rock" && computerChoice === "scissors" ||
+            humanChoice === "paper" && computerChoice === "rock" ||
+            humanChoice === "scissors" && computerChoice === "paper"
+            ) {
+            console.log(`you won! ${humanChoice} beats ${computerChoice}`)
+            playerScoreDivs[humanScore].classList.add("scored")
+            humanScore++;
+        }
 
-    if (humanChoice === "rock" && computerChoice === "scissors" ||
-        humanChoice === "paper" && computerChoice === "rock" ||
-        humanChoice === "scissors" && computerChoice === "paper"
-        ) {
-        console.log(`you won! ${humanChoice} beats ${computerChoice}`)
-        humanScore++;
-    }
+        if (humanChoice === "paper" && computerChoice === "scissors" ||
+            humanChoice === "scissors" && computerChoice === "rock" ||
+            humanChoice === "rock" && computerChoice === "paper"
+            ) {
+            console.log(`you lost! ${humanChoice} gets beaten by ${computerChoice}`)
+            computerScoreDivs[computerScore].classList.add("scored")
+            computerScore++;
+        }
+        
+        playerChoiceIcon.src = "img/question-icon.svg";
+        computerChoiceIcon.src = "img/question-icon.svg";
+    }, 1500)
+}
 
-    if (humanChoice === "paper" && computerChoice === "scissors" ||
-        humanChoice === "scissors" && computerChoice === "rock" ||
-        humanChoice === "rock" && computerChoice === "paper"
-        ) {
-        console.log(`you lost! ${humanChoice} gets beaten by ${computerChoice}`)
-        computerScore++;
+function getComputerChoice() {
+
+    // generate a random number from 0-2 (3 possibilities)
+    let result = Math.floor(Math.random() * 3)
+    
+    // choose the option that corresponds to that random number & return it
+    switch (result) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
     }
 }
 
@@ -70,22 +103,6 @@ function playGame() {
         console.log("You Win! Man triumphs over machine!");
     } else {
         console.log("You Lost! Oh man... soon computers will be taking our jobs too...");
-    }
-    
-    function getComputerChoice() {
-
-        // generate a random number from 0-2 (3 possibilities)
-        let result = Math.floor(Math.random() * 3)
-        
-        // choose the option that corresponds to that random number & return it
-        switch (result) {
-            case 0:
-                return "rock";
-            case 1:
-                return "paper";
-            case 2:
-                return "scissors";
-        }
     }
 
     function getHumanChoice() {
